0a7615103843ec077baff3404c1a6aafc568c2c0
Siba Narayana Parida <157267523+snp-007@users.noreply.github.com>
2026-05-31 03:28:18 +0530

    Refactor paths to use BASE_DIR for configuration

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
-MODEL_PATH = "app/artifacts/best_model_lightgbm.pkl"
-SCALER_PATH = "app/artifacts/scaler.pkl"
-TARGET_ENCODER_PATH = "app/artifacts/target_encoder.pkl"
-COLUMNS_PATH = "app/artifacts/model_columns.pkl"
-ENCODER_PATH = "app/artifacts/feature_label_encoders.pkl"
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+
+MODEL_PATH = BASE_DIR / "artifacts" / "best_model_lightgbm.pkl"
+SCALER_PATH = BASE_DIR / "artifacts" / "scaler.pkl"
+TARGET_ENCODER_PATH = BASE_DIR / "artifacts" / "target_encoder.pkl"
+ENCODER_PATH = BASE_DIR / "artifacts" / "feature_label_encoders.pkl"
+COLUMNS_PATH = BASE_DIR / "artifacts" / "model_columns.pkl"
